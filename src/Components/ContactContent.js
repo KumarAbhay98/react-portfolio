396306eb82f174a4d4237444008a56c338743728
Abhay Kumar <92244303+KumarAbhay98@users.noreply.github.com>
2023-03-03 13:40:03 +0530

    Update ContactContent.js

--- src/Components/ContactContent.js
+++ src/Components/ContactContent.js
@@ -14,7 +14,6 @@ const ContactContent = () => {
                         <a href="https://www.linkedin.com/in/abhay-kumar-6586b220a/" target="_blank"><BsLinkedin size={40} /></a>
                         <a href="mailto:Abhaycbr07@gmail.com" target="_blank"><SiGmail size={40} /></a>
                         <a href="https://github.com/KumarAbhay98" target="_blank"><FaGithub size={40} /></a>
-                        
                     </div>
                 </div>
             </div>
